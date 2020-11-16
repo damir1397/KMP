932eb2c6f1f952980aee86b2359ab64e3fec70e8
damir1397 <Nauto97>
2020-11-16 15:37:48 +0600

    -one commit

--- androidApp/src/main/java/kg/damir/kmm/androidApp/MainActivity.kt
+++ androidApp/src/main/java/kg/damir/kmm/androidApp/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import kg.damir.kmm.shared.Greeting
 import android.widget.TextView
 
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        tv.text = "hello"
     }
 }
